6f3af8d207916c73fbdbc12b43e96735bcf2590d
error20160113 <error404gfw@gmail.com>
2017-07-10 20:50:28 +0800

    何当共剪西窗烛

---
+++ src/main/java/org/seckill/dao/SeckillDao.java
@@ -0,0 +1,32 @@
+package org.seckill.dao;
+
+import java.util.Date;
+import java.util.List;
+
+import org.seckill.entity.Seckill;
+
+public interface SeckillDao {
+
+	/**
+	 * 减库存
+	 * @param seckillId
+	 * @param killTime
+	 * @return
+	 */
+    int reduceNumber(long seckillId, Date killTime);
+    
+    /**
+     * 根据id查询秒杀的商品信息
+     * @param seckillId
+     * @return
+     */
+    Seckill queryById(long seckillId);
+    
+    /**
+     * 根据偏移量查询秒杀商品列表
+     * @param offset
+     * @param limit
+     * @return
+     */
+    List<Seckill> queryAll(int offset,int limit);
+}
---
+++ src/main/java/org/seckill/dao/SuccessKilledDao.java
@@ -0,0 +1,22 @@
+package org.seckill.dao;
+
+import org.seckill.entity.SuccessKilled;
+
+public interface SuccessKilledDao {
+
+    /**
+     * 插入购买明细,可过滤重复
+     * @param seckillId
+     * @param userPhone
+     * @return插入的行数
+     */
+    int insertSuccessKilled(long seckillId,long userPhone);
+    
+    
+    /**
+     * 根据秒杀商品的id查询明细SuccessKilled对象(该对象携带了Seckill秒杀产品对象)
+     * @param seckillId
+     * @return
+     */
+    SuccessKilled queryByIdWithSeckill(long seckillId,long userPhone);
+}
